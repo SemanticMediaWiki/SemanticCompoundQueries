339dbb2ef20caa607cde2337ace4a7a3f4b0eeb2
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-08-14 14:19:47 +0000

    Localisation updates for extension magic words from translatewiki.net

--- SemanticCompoundQueries.i18n.magic.php
+++ SemanticCompoundQueries.i18n.magic.php
@@ -5,3 +5,11 @@ $magicWords = array();
 $magicWords['en'] = array(
 	'compound_query' => array( 0, 'compound_query' ),
 );
+
+$magicWords['ar'] = array(
+	'compound_query'   => array( '0', 'استعلام_مركب', 'compound_query' ),
+);
+
+$magicWords['nl'] = array(
+	'compound_query'   => array( '0', 'samengestelde_zoekopdracht', 'compound_query' ),
+);
