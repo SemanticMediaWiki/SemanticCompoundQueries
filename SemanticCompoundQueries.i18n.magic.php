213a86d924d1c8569fdd9fc25d416e44a3b6e864
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-10-31 17:55:47 +0000

    Localisation updates from translatewiki.net

--- SemanticCompoundQueries.i18n.magic.php
+++ SemanticCompoundQueries.i18n.magic.php
@@ -2,22 +2,32 @@
 
 $magicWords = array();
 
+/** English (English) */
 $magicWords['en'] = array(
 	'compound_query' => array( 0, 'compound_query' ),
 );
 
+/** Arabic (العربية) */
 $magicWords['ar'] = array(
-	'compound_query'   => array( '0', 'استعلام_مركب', 'compound_query' ),
+	'compound_query' => array( 0, 'استعلام_مركب', 'compound_query' ),
 );
 
+/** Egyptian Spoken Arabic (مصرى) */
 $magicWords['arz'] = array(
-	'compound_query'   => array( '0', 'استعلام_مركب', 'compound_query' ),
+	'compound_query' => array( 0, 'استعلام_مركب', 'compound_query' ),
 );
 
+/** Japanese (日本語) */
+$magicWords['ja'] = array(
+	'compound_query' => array( 0, '複合クエリー' ),
+);
+
+/** Nedersaksisch (Nedersaksisch) */
 $magicWords['nds-nl'] = array(
-	'compound_query'   => array( '0', 'samen-estelde_zeukopdrach', 'samengestelde_zoekopdracht', 'compound_query' ),
+	'compound_query' => array( 0, 'samen-estelde_zeukopdrach', 'samengestelde_zoekopdracht', 'compound_query' ),
 );
 
+/** Dutch (Nederlands) */
 $magicWords['nl'] = array(
-	'compound_query'   => array( '0', 'samengestelde_zoekopdracht', 'compound_query' ),
+	'compound_query' => array( 0, 'samengestelde_zoekopdracht' ),
 );
