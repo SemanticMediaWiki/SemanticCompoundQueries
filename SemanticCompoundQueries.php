1ed07dc1143644e90939a9bb843097d69ea78e6f
Yaron Koren <yaron@users.mediawiki.org>
2011-10-04 19:26:45 +0000

    New version: 0.3

--- SemanticCompoundQueries.php
+++ SemanticCompoundQueries.php
@@ -17,7 +17,7 @@
 
 if ( !defined( 'MEDIAWIKI' ) ) die();
 
-define( 'SCQ_VERSION', '0.2.10' );
+define( 'SCQ_VERSION', '0.3' );
 
 $wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'parserhook'][] = array(
 	'path'  => __FILE__,
