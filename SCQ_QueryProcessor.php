9d599adc716c8d043a756cc497449610890512f3
Yaron Koren <yaron@users.mediawiki.org>
2012-01-23 05:18:30 +0000

    Fixed handling of different print requests, via new method, mergeSMWPrintRequests(); also improved some tabbing

--- SCQ_QueryProcessor.php
+++ SCQ_QueryProcessor.php
@@ -29,7 +29,7 @@ class SCQQueryProcessor extends SMWQueryProcessor {
 	 */
 	public static function doCompoundQuery( Parser &$parser ) {
 		global $smwgQEnabled, $smwgIQRunningNumber;
-		
+
 		if ( !$smwgQEnabled ) {
 			return smwfEncodeMessages( array( wfMsgForContent( 'smw_iq_disabled' ) ) );
 		}
@@ -41,6 +41,7 @@ class SCQQueryProcessor extends SMWQueryProcessor {
 
 		$other_params = array();
 		$results = array();
+		$printRequests = array();
 		$queryParams = array();
 
 		foreach ( $params as $param ) {
@@ -57,40 +58,40 @@ class SCQQueryProcessor extends SMWQueryProcessor {
 				}
 			}
 		}
-		
+
 		foreach ( $queryParams as $param ) {
 			$subQueryParams = self::getSubParams( $param );
-			
+
 			if ( array_key_exists( 'format', $other_params ) && !array_key_exists( 'format', $subQueryParams ) ) {
 				$subQueryParams['format'] = $other_params['format'];
 			}
-			
+
 			$next_result = self::getQueryResultFromFunctionParams(
 				$subQueryParams,
 				SMW_OUTPUT_WIKI
 			);
 
 			$results = self::mergeSMWQueryResults( $results, $next_result->getResults() );
+			$printRequests = self::mergeSMWPrintRequests( $printRequests, $next_result->getPrintRequests() );
 		}
 
 		// Sort results so that they'll show up by page name
 		uasort( $results, array( 'SCQQueryProcessor', 'compareQueryResults' ) );
 
-		$query_result = new SCQQueryResult( $next_result->getPrintRequests(), new SMWQuery(), $results, smwfGetStore() );
-		
+		$query_result = new SCQQueryResult( $printRequests, new SMWQuery(), $results, smwfGetStore() );
+
 		if ( version_compare( SMW_VERSION, '1.6.1', '>' ) ) {
-			$printouts = $next_result->getPrintRequests();
-			SMWQueryProcessor::addThisPrintout( $printouts, $other_params );
-			$other_params = parent::getProcessedParams( $other_params, $printouts );
+			SMWQueryProcessor::addThisPrintout( $printRequests, $other_params );
+			$other_params = parent::getProcessedParams( $other_params, $printRequests );
 		}
-		
+
 		return self::getResultFromQueryResult(
 			$query_result,
 			$other_params,
 			SMW_OUTPUT_WIKI
 		);
-	}	
-	
+	}
+
 	/**
 	 * An alternative to explode() - that function won't work here,
 	 * because we don't want to split the string on all semicolons, just
@@ -104,28 +105,28 @@ class SCQQueryProcessor extends SMWQueryProcessor {
 		$sub_params = array();
 		$sub_param = '';
 		$uncompleted_square_brackets = 0;
-		
+
 		for ( $i = 0; $i < strlen( $param ); $i++ ) {
 			$c = $param[$i];
-			
+
 			if ( ( $c == ';' ) && ( $uncompleted_square_brackets <= 0 ) ) {
 				$sub_params[] = trim( $sub_param );
 				$sub_param = '';
 			} else {
 				$sub_param .= $c;
-				
+
 				if ( $c == '[' ) {
 					$uncompleted_square_brackets++;
 				}
-				
+
 				elseif ( $c == ']' ) {
 					$uncompleted_square_brackets--;
 				}
 			}
 		}
-		
+
 		$sub_params[] = trim( $sub_param );
-		
+
 		return $sub_params;
 	}
 
@@ -138,6 +139,7 @@ class SCQQueryProcessor extends SMWQueryProcessor {
 	 * @return SMWQueryResult
 	 */
 	protected static function getQueryResultFromFunctionParams( $rawparams, $outputmode, $context = SMWQueryProcessor::INLINE_QUERY, $showmode = false ) {
+		$printouts = null;
 		self::processFunctionParams( $rawparams, $querystring, $params, $printouts, $showmode );
 		return self::getQueryResultFromQueryString( $querystring, $params, $printouts, SMW_OUTPUT_WIKI, $context );
 	}
@@ -154,9 +156,8 @@ class SCQQueryProcessor extends SMWQueryProcessor {
 		if ( $result1 == null ) {
 			return $result2;
 		}
-		
 		$existing_page_names = array();
-		
+
 		foreach ( $result1 as $r1 ) {
 			// SMW 1.6+
 			if ( $r1 instanceof SMWDIWikiPage ) {
@@ -165,20 +166,35 @@ class SCQQueryProcessor extends SMWQueryProcessor {
 				$existing_page_names[] = $r1->getWikiValue();
 			}
 		}
-		
+
 		foreach ( $result2 as $r2 ) {
-			if ( $r1 instanceof SMWDIWikiPage ) {
+			if ( $r2 instanceof SMWDIWikiPage ) {
 				$page_name = $r2->getDBkey();
 			} else {
 				$page_name = $r2->getWikiValue();
 			}
-			
+
 			if ( ! in_array( $page_name, $existing_page_names ) ) {
 				$result1[] = $r2;
 			}
 		}
-		
+
 		return $result1;
+	}
+
+	protected static function mergeSMWPrintRequests( $printRequests1, $printRequests2 ) {
+		$existingPrintoutLabels = array();
+		foreach ( $printRequests1 as $p1 ) {
+			$existingPrintoutLabels[] = $p1->getLabel();
+		}
+
+		foreach ( $printRequests2 as $p2 ) {
+			$label = $p2->getLabel();
+			if ( ! in_array( $label, $existingPrintoutLabels ) ) {
+				$printRequests1[] = $p2;
+			}
+		}
+		return $printRequests1;
 	}
 
 	/**
@@ -192,24 +208,24 @@ class SCQQueryProcessor extends SMWQueryProcessor {
 	 */
 	protected static function getQueryResultFromQueryString( $querystring, array $params, $extraprintouts, $outputmode, $context = SMWQueryProcessor::INLINE_QUERY ) {
 		wfProfileIn( 'SCQQueryProcessor::getQueryResultFromQueryString' );
-		
+
 		if ( version_compare( SMW_VERSION, '1.6.1', '>' ) ) {
 			SMWQueryProcessor::addThisPrintout( $extraprintouts, $params );
 			$params = self::getProcessedParams( $params, $extraprintouts, false );
 		}
-		
+
 		$query = self::createQuery( $querystring, $params, $context, null, $extraprintouts );
 		$query_result = smwfGetStore()->getQueryResult( $query );
-		
+
 		foreach ( $query_result->getResults() as $wiki_page ) {
 			$wiki_page->display_options = $params;
 		}
 
 		wfProfileOut( 'SCQQueryProcessor::getQueryResultFromQueryString' );
-		
+
 		return $query_result;
 	}
-	
+
 	/**
 	 * Matches getResultFromQueryResult() from SMWQueryProcessor,
 	 * except that formats of type 'debug' and 'count' aren't handled.
@@ -231,13 +247,13 @@ class SCQQueryProcessor extends SMWQueryProcessor {
 		else {
 			$format = self::getResultFormat( $params );
 		}
-		
+
 		$printer = self::getResultPrinter( $format, $context, $res );
 		$result = $printer->getResult( $res, $params, $outputmode );
-		
+
 		wfProfileOut( 'SCQQueryProcessor::getResultFromQueryResult' );
-		
+
 		return $result;
 	}
-	
+
 }
