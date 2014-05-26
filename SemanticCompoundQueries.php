ae8f8250b1a7df0e8f623816087aef741c680064
yaron <yaron57@gmail.com>
2014-05-26 19:13:05 +0400

    New version: 0.4
    Change-Id: I01596b3e5c8a64605658552e2bbeb3984bb5f2be

--- SemanticCompoundQueries.php
+++ SemanticCompoundQueries.php
@@ -17,7 +17,7 @@
 
 if ( !defined( 'MEDIAWIKI' ) ) die();
 
-define( 'SCQ_VERSION', '0.4.0' );
+define( 'SCQ_VERSION', '0.4' );
 
 $wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'parserhook'][] = array(
 	'path'  => __FILE__,
