e351a6f31e376c7d81cfbfb0cf7c42684b2abf5e
PeterTheOne <petertheone@gmail.com>
2016-07-01 14:47:59 +0200

    add testGetSubParams

--- tests/phpunit/SCQ_QueryProcessorTest.php
+++ tests/phpunit/SCQ_QueryProcessorTest.php
@@ -9,6 +9,23 @@
 
 class SCQQueryProcessorTest extends \PHPUnit_Framework_TestCase
 {
+	/**
+	 * Call protected/private method of a class.
+	 *
+	 * @param object &$object    Instantiated object that we will run method on.
+	 * @param string $methodName Method name to call
+	 * @param array  $parameters Array of parameters to pass into method.
+	 *
+	 * @return mixed Method return.
+	 */
+	public function invokeMethod( &$object, $methodName, array $parameters = array() ) {
+		$reflection = new \ReflectionClass( get_class( $object ) );
+		$method = $reflection->getMethod( $methodName );
+		$method->setAccessible( true );
+
+		return $method->invokeArgs( $object, $parameters );
+	}
+
 	public function testSeparateParams() {
 		$params = [
 			'[[Something in Square Brackets]]',
@@ -34,5 +51,22 @@ class SCQQueryProcessorTest extends \PHPUnit_Framework_TestCase
 		];
 
 		$this->assertEquals( $expected, SCQQueryProcessor::separateParams( $params ) );
+	}
+
+	public function testGetSubParams() {
+		$param = 'split; trimsplit ;split;[don\'t;split];split;[[[don\'t;split]]];split;[[don\'t;split';
+		$expected = [
+			'split',
+			'trimsplit',
+			'split',
+			'[don\'t;split]',
+			'split',
+			'[[[don\'t;split]]]',
+			'split',
+			'[[don\'t;split',
+		];
+
+		$actual = $this->invokeMethod( new SCQQueryProcessor(), 'getSubParams', array( $param ) );
+		$this->assertEquals( $expected, $actual );
 	}
 }
